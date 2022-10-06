823229f44d2277703e0a737d2a0ce398f3e03e43
Leland Johnson <lpjohnsonlpjohnson@gmail.com>
2022-10-05 22:45:41 -0500

    File setup

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.js",
+
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
+};
